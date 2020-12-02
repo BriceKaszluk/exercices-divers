36ae47216e1923599c5be72ed0e4193f21df63b8
Jean-Denis Vauguet <jd@vauguet.fr>
2020-12-02 14:31:30 +0100

    Exo 1

--- js/exo1.js
+++ js/exo1.js
@@ -18,3 +18,28 @@ Notions
 */
 
 const items = ['Sunglasses', 'Suit', 'Business card', 'Jet ski'];
+
+/**
+ * 
+ */
+items.forEach(function(item) {
+  console.log(item);
+  
+  // 1. on va avoir besoin, pour appendChild, d'un nœud / élément
+  //    donc on le crée (en terme de type de données, c'est un objet JS)
+  let elt = document.createElement('li'); // on crée un Node du DOM
+  elt.textContent = item;
+
+  // 2. on sélectionne l'endroit pré-existant du HTML où on fera
+  //    l'injection de contenu
+  let target = document.getElementById('result');
+
+  // 3. on utilise appendChild pour modifier le DOM (donc on travaille
+  //    dans le contexte JS, pas HTML)
+  target.appendChild(elt);
+
+
+});
+
+// items.forEach(generateLi);
+// function generateLi() { ... }
